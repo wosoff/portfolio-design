c463afa0776d64e1431ae0e1161ea55a1f921075
wosoff <wosoff@naver.com>
2020-02-01 12:55:46 +0900

    contact copy window open-close event handling

--- src/views/components/contact-logo/contact-logo.js
+++ src/views/components/contact-logo/contact-logo.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import ClassNames from 'classnames'
+import ContactCopyWindow from '../test-contact-copy-window/contact-copy-window'
 import './style/contact-logo.sass'
 
 export default class ContactLogo extends Component {
@@ -11,9 +12,23 @@ export default class ContactLogo extends Component {
     this.timerID = 0
     this.duration = 50
     this.state = {
-      isOnLogoContact: false
+      isOnLogoContact: false,
+      isOnContactCopyWindow: false,
+      didClickCloseBtnToCopyWindow: false
     }
+    this.handleLogoClick = this.handleLogoClick.bind(this)
+    this.handleCloseCopyWindow = this.handleCloseCopyWindow.bind(this)
+  }
 
+  handleLogoClick() {
+    this.setState({
+      isOnContactCopyWindow: true,
+      didClickCloseBtnToCopyWindow: false
+    })
+  }
+
+  handleCloseCopyWindow() {
+    this.setState({didClickCloseBtnToCopyWindow: true})
   }
 
   componentDidMount() {
@@ -29,43 +44,29 @@ export default class ContactLogo extends Component {
   }
 
   render() {
-    const {isOnLogoContact} = this.state
+    const {
+      isOnLogoContact, isOnContactCopyWindow, didClickCloseBtnToCopyWindow
+    } = this.state
+
     const className = ClassNames('contact-logo', {
       'show-contact-logo': isOnLogoContact
     })
   
     return (
-      <div className={className}>
-        <a
-          href="/"
-        >
-          <img 
-            src="public/assets/images/contact-logo.gif"
-            alt="Contact Logo"
-          />
-        </a>
+      <div 
+        className={className}
+      >
+        <img 
+          onClick={this.handleLogoClick}
+          src="public/assets/images/contact-logo.gif"
+          alt="Contact Logo"
+        />
+        <ContactCopyWindow 
+          isOnContactCopyWindow={isOnContactCopyWindow}
+          didClickCloseBtnToCopyWindow={didClickCloseBtnToCopyWindow}
+          handleCloseCopyWindow={this.handleCloseCopyWindow}
+        />
       </div>
     )
   }
 }
-
-// export default function ContactLogo(props) {
-//   const {state} = props
-
-//   const className = ClassNames('contact-logo', {
-//     'show-contact-logo': state.onLogoContact
-//   })
-  
-//   return (
-//     <div className={className}>
-//       <a
-//         href="/"
-//       >
-//         <img 
-//           src="public/assets/images/contact-logo.gif"
-//           alt="Contact Logo"
-//         />
-//       </a>
-//     </div>
-//   )
-// }
