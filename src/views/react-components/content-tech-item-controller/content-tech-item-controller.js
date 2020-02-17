c8e36e47437ad7fe88d12a1219b960dfacc144cc
wosoff <wosoff@naver.com>
2020-02-17 14:10:42 +0900

    test 1

--- src/views/react-components/content-tech-item-controller/content-tech-item-controller.js
+++ src/views/react-components/content-tech-item-controller/content-tech-item-controller.js
@@ -1,53 +1,66 @@
 import './style/content-tech-item-controller.sass'
 import React, {useState} from 'react'
-import TechRangeGradientBar from '../tech-range-gradient-bar/tech-range-gradient-bar'
-import ImageGridViewer from '../image-grid-viewer/image-grid-viewer'
+import CommonTechRangeBar from '../common-tech-range-bar/common-tech-range-bar'
+import ImageGrigViewer from '../image-grid-viewer/image-grid-viewer'
 import selectLanguage from '../../helpers/selectLanguage'
 
 /**
- * @param {{ techItemInfo: {title: string; level: any;}; gridViewerItems: {src: any; viewer: any}[];}} props
+ * @param {{ materializations: {viewer: any; src: string; description: string; }[]; setShowingGrid: Function; }} props
+ */
+function ShowingGridButton(props) {
+  const {materializations, setShowingGrid} = props
+  if (materializations.length === 0) {
+    return <></>
+  }
+  return (
+    <>
+      <button 
+        className="show-content-tech-items"
+        onClick={() => {setShowingGrid(true)}}
+      >
+        {
+          selectLanguage({en: 'See Materialization', ko: '기술 구현 보기'})
+        }
+      </button>
+    </>
+  )
+}
+
+/**
+ * @param {{techItems: {title: string; isAdvanced: boolean; materializations: any[]}[]}} props
  */
 export default function ContentTechItemController(props) {
-  const {techItemInfo, gridViewerItems} = props
-  const {title, level} = techItemInfo
-  const [isShowingGrid, setShowingGrid] = useState(false)
-
-  function ShowingGridButton() {
-    if (gridViewerItems.length === 0) {
-      return <></>
-    }
+  // @ts-ignore
+  const {techItems} = props
+  
+  const controllingTechItems = techItems.map((item) => {
+    const {title, isAdvanced, materializations} = item
+    const [isShowingGrid, setShowingGrid] = useState(false)
+    
     return (
       <>
-        <button 
-          className="show-content-tech-items"
-          onClick={() => {setShowingGrid(true)}}
-        >
+        <div className="content-tech-item-controller">
+          <h3 className="item-controller-title">{title}</h3>
+          <div className="item-controller-level">
+          <CommonTechRangeBar isAdvanced={isAdvanced}/>
+        </div>
+        <ShowingGridButton 
+          materializations={materializations}
+          setShowingGrid={setShowingGrid}
+        />
+        </div>
+        <div className="content-tech-item-grid">
           {
-            selectLanguage({en: 'See Materialization', ko: '기술 구현 보기'})
+            isShowingGrid === true && 
+            <ImageGrigViewer 
+              closeGridViewer={setShowingGrid}
+              gridViewerItems={materializations}
+           />
           }
-        </button>
+       </div>
       </>
     )
-  }
+  })
 
-  return (
-  <>
-    <div className="content-tech-item-controller">
-      <h3 className="item-controller-title">{title}</h3>
-      <div className="item-controller-level">
-        <TechRangeGradientBar level={level}/>
-      </div>
-      <ShowingGridButton />
-    </div>
-    <div className="content-tech-item-grid">
-      {
-        isShowingGrid === true && 
-        <ImageGridViewer 
-          closeGridViewer={setShowingGrid}
-          gridViewerItems={gridViewerItems}
-        />
-      }
-    </div>
-  </>
-  )
+  return (<>{controllingTechItems}</>)
 }
